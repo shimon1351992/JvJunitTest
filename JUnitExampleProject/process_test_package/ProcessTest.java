de6f19fc48afc2dba6186556d10d1df8e76e5425
shimon1351992 <73971771+shimon1351992@users.noreply.github.com>
2021-06-20 21:22:51 +0300

    Update ProcessTest.java

--- JUnitExampleProject/process_test_package/ProcessTest.java
+++ JUnitExampleProject/process_test_package/ProcessTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,17 +19,26 @@ import org.junit.jupiter.api.Test;
 
 import Process.ProcessRun;
 
+
+
+
+
 /**
  * @author Itai
  *
  */
 class ProcessTest {
 
+	String output ;
+	ProcessRun process ;
+	String[] command = {"C:\\QA_Tirgol\\CshConsoleAPI\\bin\\Debug\\CshConsoleAPI.exe",""};
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	/**
@@ -38,11 +48,14 @@ class ProcessTest {
 	static void tearDownAfterClass() throws Exception {
 	}
 
-	/**
+	/** 
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		System.out.println("Setup before every test");
+		output = "";
+		process = new ProcessRun();
 	}
 
 	/**
@@ -50,9 +63,13 @@ class ProcessTest {
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
+	System.out.println("After every test test");
 	}
 
 
+	/***
+	 * Test the stack trace print is working.
+	 */
 	@Test
 	void testTrace() {	
 		try {
@@ -64,28 +81,101 @@ class ProcessTest {
 			e.printStackTrace(pw);
 			String trace1 = sw.toString();
 			System.out.print(trace1);
-			fail(trace1);
-
+			
+			Assert.assertTrue(!trace1.isEmpty());
 		}
 	}
-	
-	
-	
+
+
+	/***
+	 * Test operating system command line.
+	 * Run command line sub process.
+	 */	
 	@Test
 	void testCmdDir() {
-		ProcessRun process = new ProcessRun();;
 		
 		try {
 			String[] dir_rist = {"dir", "c:\\"};
 			process.Execute(dir_rist);
-		} catch (IOException e) {
+			process = null;
+		} catch (Exception e) {
 			fail("Unable to run dir");
 		}
+		return;
 	}
 	
+	
+	/***
+	 * Test the console application runs.
+	 */
 	@Test
 	void testConsole() {
-		
+		try {
+			ProcessRun process = new ProcessRun();
+			output = process.Run(command);
+			System.out.println(output);
+						
+			output = process.Exit();			
+		}
+		catch (Exception e) {
+			//	In case of exception.
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			//	Read the trace information.
+			e.printStackTrace(pw);
+			String trace = sw.toString();
+			
+			//	Write the trace to console and fail the test.
+			System.out.print(trace);
+			Assert.assertTrue(!trace.isEmpty());
+		}
+		return;
 	}
+
 	
+	/***
+	 * Test the console application runs 
+	 * and execute command-line multiple commands.
+	 */
+	@Test
+	void testCommands() {
+		
+		try {
+			output = process.Run(command);
+			Assert.assertTrue(output.contentEquals("\n>"));
+			
+			output = process.Command("echo(Hello world)\n");
+			Assert.assertTrue(output.contentEquals("Hello world\r\n\n>"));
+			
+			output = process.Command("mul(4,6)\n");
+			Assert.assertTrue(output.contentEquals("24\r\n\n>"));
+			
+			output = process.Command("add(4,6)\n");
+			Assert.assertTrue(output.contentEquals("10\r\n\n>"));
+			
+			output = process.Command("sub(8,6)\n");
+			Assert.assertTrue(output.contentEquals("2\r\n\n>"));
+			
+			output = process.Command("div(4,2)\n");
+			Assert.assertTrue(output.contentEquals("2\r\n\n>"));
+
+			output = process.Exit();			
+		}
+		catch (Exception e) {
+			//	In case of exception.
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			//	Read the trace information.
+			e.printStackTrace(pw);
+			String trace = sw.toString();
+			
+			//	Write the trace to console and fail the test.
+			System.out.print(trace);
+			Assert.assertTrue(!trace.isEmpty());
+		}
+		return;
+	}
+
+	
+
 }
